4b4ce8a545e9d60b6bdb184ef1e440666740604d
Caio Vieira <caioviier@gmail.com>
2023-08-02 17:00:20 -0300

    Update code with Vue!

--- maxmilian/first-app/app.js
+++ maxmilian/first-app/app.js
@@ -1,3 +1,6 @@
+//! PURE JS VERSION 
+/*
+
 const buttonEl = document.querySelector('button');
 const inputEl = document.querySelector('input');
 const listEl = document.querySelector('ul');
@@ -10,4 +13,22 @@ function addGoal() {
   inputEl.value = "";
 }
 
-buttonEl.addEventListener('click', addGoal);
+buttonEl.addEventListener('click', addGoal); 
+
+*/
+//& VUE JS VERSION
+
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: ''
+    }
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue)
+      this.enteredValue = ''
+    }
+  }
+}).mount("#app")
